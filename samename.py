f81f30b456d810422fbf4cb18032e3303ad15ba6
trastolillo <trastolillo@tutanota.com>
2021-07-31 00:18:28 +0200

    refactorizado

--- samename.py
+++ samename.py
@@ -4,8 +4,10 @@ import re
 import mimetypes
 
 # Variables
-argumentos = sys.argv
-extensiones_subtitulos = ('.srt', '.sub', '.idx')
+
+
+lista_videos = [os.path.splitext(f)[0] for f in os.listdir(
+    carpeta) if 'video' in mimetypes.guess_type(f)[0]]
 carpeta: str = ''
 subtitulos = []
 
@@ -16,16 +18,16 @@ def salir_con_error(mensaje):
 
 
 # Validación de argumentos
-if len(argumentos) > 2:
-    salir_con_error('\nSólo se admite un argumento (carpeta de los archivos)')
-
-try:
-    carpeta = argumentos[1] if len(argumentos) == 2 else os.getcwd()
-except FileNotFoundError:
-    salir_con_error('No existe la carpeta')
-
-
-# Funciones
+def validar_argumentos():
+    argumentos = sys.argv
+    if len(argumentos) > 2:
+        salir_con_error(
+            '\nSólo se admite un argumento (carpeta de los archivos)')
+    try:
+        global carpeta
+        carpeta = argumentos[1] if len(argumentos) == 2 else os.getcwd()
+    except FileNotFoundError:
+        salir_con_error('No existe la carpeta')
 
 
 def extraer_episodio(file_name: str):
@@ -40,12 +42,6 @@ def extraer_episodio(file_name: str):
         sys.exit(1)
 
 
-lista_videos = [os.path.splitext(f)[0] for f in os.listdir(
-    carpeta) if 'video' in mimetypes.guess_type(f)[0]]
-
-print(lista_videos)
-
-
 def extraer_nombre_video(subtitulo: str, video: str):
     if extraer_episodio(video) == extraer_episodio(subtitulo):
         return video
@@ -53,17 +49,29 @@ def extraer_nombre_video(subtitulo: str, video: str):
         return 0
 
 
-# Cambio de nombres
-os.chdir(carpeta)
-for index, f in enumerate(os.listdir()):
-    file_name, file_ext = os.path.splitext(f)
-    if file_ext in extensiones_subtitulos and len(lista_videos) > 0:
-        for video in lista_videos:
-            nombre_video = extraer_nombre_video(f, video)
-            print('62:', f, video)
-            if nombre_video and nombre_video != file_name:
-                print('renombrar', file_name, 'con', nombre_video)
-                nuevo_nombre = f'{nombre_video}{file_ext}'
-                os.rename(f, nuevo_nombre)
-                lista_videos.remove(video)
-                print(f'renombrado {nuevo_nombre}', len(lista_videos))
+def renombrar(f, file_name, file_ext):
+    for video in lista_videos:
+        nombre_video = extraer_nombre_video(f, video)
+        if nombre_video and nombre_video != file_name:
+            print('renombrar', file_name, 'con', nombre_video)
+            nuevo_nombre = f'{nombre_video}{file_ext}'
+            os.rename(f, nuevo_nombre)
+            lista_videos.remove(video)
+            print(f'renombrado {nuevo_nombre}')
+
+
+def main():
+    validar_argumentos()
+    os.chdir(carpeta)
+    extensiones_subtitulos = ('.srt', '.sub', '.idx')
+    for f in os.listdir():
+        if file_ext in extensiones_subtitulos and len(lista_videos) > 0:
+            file_name, file_ext = os.path.splitext(f)
+            renombrar(f, file_name, file_ext)
+        else:
+            print('Nada que renombrar')
+            sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
